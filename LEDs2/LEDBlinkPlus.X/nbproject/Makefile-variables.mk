f04e1145596e691adc95a06aa714319f48872119
Nicolas <nicostib12@gmail.com>
2026-02-27 14:43:52 -0500

    ADD: Configuracion de registros (Oscilador y Timer0), pines y una variable TIEMPO

---
+++ LEDs2/LEDBlinkPlus.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LEDBlinkPlus.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LEDBlinkPlus.X.production.hex
