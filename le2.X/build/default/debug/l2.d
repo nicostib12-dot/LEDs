ef81cf38b8c7b50bd5ab968a332a9555abc45bb2
davidkordonez <davidkordonez@unicauca.edu.co>
2026-03-01 18:42:18 -0500

    se crea un codigo con la intencion de cumplir el segundo requeriemiento pero este sin usar timer si no un anidado para el tiempo exacto

---
+++ le2.X/build/default/debug/l2.d
@@ -0,0 +1,1 @@
+build/default/debug\l2.i: l2.asm
---
+++ le2.X/l2.asm
@@ -0,0 +1,102 @@
+#include <xc.inc>
+
+CONFIG FOSC = INTOSCIO_EC
+CONFIG WDT = OFF
+CONFIG LVP = OFF
+CONFIG PBADEN = OFF
+
+PSECT resetVec, class=CODE, reloc=2
+ORG 0x00
+GOTO inicio
+
+PSECT main_code, class=CODE, reloc=2
+
+inicio:
+    CLRF TRISB
+    CLRF LATB
+
+principal:
+    CALL prender1
+    CALL prender2
+    GOTO principal
+
+;============================
+; 5 parpadeos 1 segundo
+;============================
+prender1:
+    MOVLW 5
+    MOVWF Contador1
+
+bucle1:
+    BSF LATB,0
+    CALL timer1
+
+    BCF LATB,0
+    CALL timer1
+
+    DECFSZ Contador1, F
+    GOTO bucle1
+    RETURN
+
+;============================
+; 2 parpadeos 2 segundos
+;============================
+prender2:
+    MOVLW 2
+    MOVWF Contador2
+
+bucle2:
+    BSF LATB,0
+    CALL timer2
+
+    BCF LATB,0
+    CALL timer2
+
+    DECFSZ Contador2, F
+    GOTO bucle2
+    RETURN
+
+;============================
+; Delay 1 segundo (8MHz)
+;============================
+timer1:
+    MOVLW 32
+    MOVWF Cont1
+
+Loop1:
+    MOVLW 250
+    MOVWF Cont2
+
+Loop2:
+    MOVLW 250
+    MOVWF Cont3
+
+Loop3:
+    DECFSZ Cont3, F
+    GOTO Loop3
+
+    DECFSZ Cont2, F
+    GOTO Loop2
+
+    DECFSZ Cont1, F
+    GOTO Loop1
+
+    RETURN
+
+;============================
+; Delay 2 segundos
+;============================
+timer2:
+    CALL timer1
+    CALL timer1
+    RETURN
+
+;============================
+; VARIABLES
+;============================
+PSECT udata
+Contador1: DS 1
+Contador2: DS 1
+Cont1: DS 1
+Cont2: DS 1
+Cont3: DS 1
