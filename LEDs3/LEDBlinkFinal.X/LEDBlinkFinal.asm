c71bce9f22c0b5cc7f253a400fb62feaa0c70392
Nicolas <nicostib12@gmail.com>
2026-03-02 11:32:46 -0500

    BREAKING CHANGE: Arregle la estructura dentro del main para que tuviera continuidad con las secuencias, ademas de cambios de syntax que no eran del todo comprendido por el copilador, como los acces y el cambio de nombre a numero de bit del INTCONT, ade mas de agregar las interrupciones

--- LEDs3/LEDBlinkFinal.X/LEDBlinkFinal.asm
+++ LEDs3/LEDBlinkFinal.X/LEDBlinkFinal.asm
@@ -26,142 +26,144 @@ CONFIG  PBADEN = OFF
 ; VARIABLES (Access Bank)
 ;============================
 PSECT udata_acs
-
-MODO        DS 1   ; 0?3 secuencia actual
-VELOCIDAD   DS 1   ; 0=rápida  1=lenta
-INDICE      DS 1   ; posición dentro del efecto
-TICK        DS 1   ; contador base 10 ms
-FLAG_UPDATE DS 1   ; indica cuándo actualizar LEDs
-DB_MODE     DS 1   ; debounce botón secuencia
-DB_SPEED    DS 1   ; debounce botón velocidad
+MODO:        DS 1   ;  secuencia actual
+VELOCIDAD:   DS 1   ; 0=rápida  1=lenta
+INDICE:      DS 1   ; posición dentro del efecto
+TICK:        DS 1   ; contador base 10 ms
+FLAG_UPDATE: DS 1   ; indica cuándo actualizar LEDs
+DB_MODE:     DS 1   ; debounce botón secuencia
+DB_SPEED:    DS 1   ; debounce botón velocidad
 
 ;---------------------------------------------------------
 ; VECTOR DE RESET
 ;---------------------------------------------------------
-PSECT resetVec,class=CODE,reloc=2
+PSECT resetVec
 ORG 0x0000
 GOTO INIT
 
 ;---------------------------------------------------------
 ; VECTOR DE INTERRUPCIÓN (alta prioridad)
 ;---------------------------------------------------------
-PSECT intVec,class=CODE,reloc=2
+PSECT intVec
 ORG 0x0008
+ GOTO ISR
  
- ;---------------------------------------------------------
-; CÓDIGO PRINCIPAL
-;---------------------------------------------------------
-
-PSECT code,class=CODE,reloc=2
-
 ;---------------------------------------------------------
 ; INICIALIZACIÓN
 ;---------------------------------------------------------
-
+PSECT code
 INIT:
 
     ; PORTD como salida (LEDs)
-    CLRF LATD
-    CLRF PORTD
-    CLRF TRISD
+    CLRF LATD,A
+    CLRF PORTD,A
+    CLRF TRISD,A
 
     ; RB0 y RB1 como entradas (botones)
-    BSF TRISB,0
-    BSF TRISB,1
+    BSF TRISB,0,A
+    BSF TRISB,1,A
 
     ; Inicializar variables
-    CLRF TICK
-    CLRF MODO
-    CLRF VELOCIDAD
-    CLRF FLAG_UPDATE
+    CLRF TICK,A
+    CLRF MODO,A
+    CLRF VELOCIDAD,A
+    CLRF FLAG_UPDATE,A
+    CLRF DB_MODE,A
+    CLRF DB_SPEED,A
     MOVLW 0x01
-    MOVWF INDICE
+    MOVWF INDICE,A
 
 ;---------------------------------------------------------
 ; CONFIGURAR TIMER0
 ;---------------------------------------------------------
 
-    MOVLW b'10000111'      ; Timer0 ON, 16bit, prescaler 256
-    MOVWF T0CON
+    MOVLW 0b10000111     ; Timer0 ON, 16bit, prescaler 256
+    MOVWF T0CON,A
 
     ; Precarga para ~10ms (depende del cristal)
     MOVLW 0x63
-    MOVWF TMR0H
+    MOVWF TMR0H,A
     MOVLW 0xC0
-    MOVWF TMR0L
+    MOVWF TMR0L,A
 
-    BSF INTCON,TMR0IE      ; Habilita Timer0
-    BSF INTCON,GIE         ; Habilita interrupciones globales
+    BSF INTCON,5,A       ; habilita Timer0
+    BSF INTCON,7,A        ; Habilita interrupciones globales
 
 ;---------------------------------------------------------
 ; CONFIGURAR INTERRUPCIONES EXTERNAS
 ;---------------------------------------------------------
 
-    BSF INTCON,INT0IE
-    BCF INTCON2,INTEDG0    ; Flanco descendente
+    BSF INTCON,4,A
+    BCF INTCON2,6,A    ; Flanco descendente
 
-    BSF INTCON3,INT1IE
-    BCF INTCON2,INTEDG1    ; Flanco descendente
+    BSF INTCON3,3,A
+    BCF INTCON2,7,A    ; Flanco descendente
 
 ;---------------------------------------------------------
 ; BUCLE PRINCIPAL
 ;---------------------------------------------------------
 
 MAIN:
+    ; Espera hasta que Timer0 indique actualización
+    BTFSS FLAG_UPDATE,0,A
+    GOTO MAIN
 
-    MOVF MODO,W
+    BCF FLAG_UPDATE,0,A
+
+    MOVF MODO,W,A
     SUBLW 0x00
     BZ SEQ1
     
-    MOVF MODO,W
+    MOVF MODO,W,A
     SUBLW 0x01
     BZ SEQ2
 
-    MOVF MODO,W
+    MOVF MODO,W,A
     SUBLW 0x02
     BZ SEQ3
 
     GOTO SEQ4
+
     
 ;---------------------------------------------------------
-; SECUENCIA 1 ? Corrimiento derecha
+; SECUENCIA 1  Corrimiento derecha
 ;---------------------------------------------------------
 
 SEQ1:
-    MOVF INDICE,W
-    MOVWF LATD
+    MOVF INDICE,W,A
+    MOVWF LATD,A
 
-    RLNCF INDICE,F
+    RLNCF INDICE,F,A
     MOVLW 0x10
-    CPFSEQ INDICE
+    CPFSEQ INDICE,A
     RETURN
 
     MOVLW 0x01
-    MOVWF INDICE
+    MOVWF INDICE,A
     RETURN
 ;---------------------------------------------------------
-; SECUENCIA 2 ? Corrimiento izquierda
+; SECUENCIA 2 Corrimiento izquierda
 ;---------------------------------------------------------
 
 SEQ2:
-    MOVF INDICE,W
-    MOVWF LATD
+    MOVF INDICE,W,A
+    MOVWF LATD,A
 
-    RRNCF INDICE,F
+    RRNCF INDICE,F,A
     MOVLW 0x00
-    CPFSEQ INDICE
+    CPFSEQ INDICE,A
     RETURN
 
     MOVLW 0x08
-    MOVWF INDICE
+    MOVWF INDICE,A
     RETURN
 ;---------------------------------------------------------
-; SECUENCIA 3 ? Par/Impar
+; SECUENCIA 3  Par/Impar
 ;---------------------------------------------------------
 
 SEQ3:
     MOVLW 0x0A
-    MOVWF LATD
+    MOVWF LATD,A
     RETURN
 
 ;---------------------------------------------------------
@@ -169,6 +171,88 @@ SEQ3:
 ;---------------------------------------------------------
 
 SEQ4:
-    COMF LATD,F
+    COMF LATD,F,A
+
+;---------------------------------------------------------
+; INTERRUPCIONES
+;---------------------------------------------------------
+
+ISR:
+
+    ; Timer0
+    BTFSC INTCON,2,A
+    CALL ISR_TIMER0
+
+    ; INT0
+    BTFSC INTCON,4,A
+    CALL ISR_INT0
+
+    ; INT1
+    BTFSC INTCON3,3,A
+    CALL ISR_INT1
+
+    RETFIE
+
+;---------------------------------------------------------
+; ISR TIMER0
+;---------------------------------------------------------
+
+ISR_TIMER0:
+
+    BCF INTCON,2,A
+
+    ; Recargar Timer0
+    MOVLW 0x63
+    MOVWF TMR0H,A
+    MOVLW 0xC0
+    MOVWF TMR0L,A
+
+    INCF TICK,F,A
+
+    ; Verificar velocidad
+    BTFSC VELOCIDAD,0,A
+    GOTO RAPIDA
+
+LENTA:
+    MOVLW 30
+    CPFSGT TICK,A
+    RETURN
+    GOTO ACTUALIZAR
+
+RAPIDA:
+    MOVLW 10
+    CPFSGT TICK,A
+    RETURN
+
+ACTUALIZAR:
+    CLRF TICK,A
+    BSF FLAG_UPDATE,0,A
+    RETURN
+
+;---------------------------------------------------------
+; ISR INT0 Cambiar secuencia
+;---------------------------------------------------------
+
+ISR_INT0:
+
+    BCF INTCON,4,A
+
+    INCF MODO,F,A
+    MOVLW 4
+    CPFSEQ MODO,A
+    RETURN
+
+    CLRF MODO,A
+    RETURN
+
+;---------------------------------------------------------
+; ISR INT1  Cambiar velocidad
+;---------------------------------------------------------
+
+ISR_INT1:
+
+    BCF INTCON3,3,A
+    COMF VELOCIDAD,F,A
+    RETURN
 
 END
---
+++ LEDs3/LEDBlinkFinal.X/build/default/debug/LEDBlinkFinal.d
@@ -0,0 +1,1 @@
+build/default/debug\LEDBlinkFinal.i: LEDBlinkFinal.asm
